a6dd7b5fe84f74afec81262b1eccb46bb1e8ddd6
Daniel Hansen <dph1108@gmail.com>
2018-05-04 16:06:49 +0200

    Lavet testcases til MonitorTest, både at den flyver ind i systemet, og den flyver ud af systemet

--- ATM/ATM.Unit.Test/MonitorTest.cs
+++ ATM/ATM.Unit.Test/MonitorTest.cs
@@ -1,4 +1,5 @@
-﻿using ATMClasses.Data;
+﻿using System;
+using ATMClasses.Data;
 using ATMClasses.Filtering;
 using ATMClasses.Interfaces;
 using NUnit.Framework;
@@ -12,27 +13,48 @@ namespace ATM.Unit.Test
     {
         private ITracks _track;
         private Monitor _uut;
-
+        
         [SetUp]
         public void Setup()
         {
-            _track = Substitute.For<ITracks>();
+            _track = new TrackData();
+
             _uut = new Monitor(_track);
         }
 
-        public void Action()
+        public void Action(int x, int y, int z)
         {
-            _track.X = 10000;
-            _track.Y = 90000;
+            _track.X = x;
+            _track.Y = y;
+            _track.Altitude = z;
+            
+            _uut.InView = _uut.MonitorFlight(_track);
         }
-        
 
-        [Test]
-        public void Track_Is_In_Range()
+        [TestCase(10000, 10000, 500, true)]
+        [TestCase(90000, 90000, 500, true)]
+        [TestCase(10000, 10000, 20000, true)]
+        [TestCase(90000, 90000, 20000, true)]
+        public void Track_Is_In_Range(int x, int y, int alt, bool view)
         {
-            Action();
-            Assert.That(_uut,Is.EqualTo(true));
+            Action(x, y , alt);
+            Assert.AreEqual(_uut.InView, view);
         }
-        
+
+        [TestCase(10000, 10000, 499, false)]
+        [TestCase(90000, 90000, 499, false)]
+        [TestCase(10000, 10000, 20001, false)]
+        [TestCase(90000, 90000, 20001, false)]
+        [TestCase(10000, 9999, 500, false)]
+        [TestCase(90000, 90001, 500, false)]
+        [TestCase(9999, 10000, 2000, false)]
+        [TestCase(90001, 90000, 2000, false)]
+        public void Track_Is_Out_Of_Range(int x, int y, int alt, bool view)
+        {
+            Action(x, y, alt);
+            Assert.AreEqual(_uut.InView, view);
+        }
+
+
     }
 }
--- ATM/ATMClasses/Filtering/Monitor.cs
+++ ATM/ATMClasses/Filtering/Monitor.cs
@@ -1,12 +1,28 @@
-﻿using ATMClasses.Interfaces;
+﻿using System;
+using System.Collections.Generic;
+using ATMClasses.Data;
+using ATMClasses.Interfaces;
 
 namespace ATMClasses.Filtering
 {
     public class Monitor : IMonitors
     {
+        public bool InView { get; set; }
         public Monitor(ITracks track)
         {
+            InView = false;
+            InView = MonitorFlight(track);
+        }
 
+        public bool MonitorFlight(ITracks track)
+        {
+            //De rette koords
+            return track.X <= 90000
+                   && track.X >= 10000
+                   && track.Y <= 90000
+                   && track.Y >= 10000
+                   && track.Altitude >= 500
+                   && track.Altitude <= 20000;
         }
     }
 }
