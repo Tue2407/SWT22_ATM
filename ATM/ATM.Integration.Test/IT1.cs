af5931954e2f7207c427be49c9bf9710a6c72f7b
Tue Jensen <the_tue@hotmail.com>
2018-05-11 10:59:52 +0200

    IT1/2 work started.

--- ATM/ATM.Integration.Test/IT1.cs
+++ ATM/ATM.Integration.Test/IT1.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using ATMClasses;
+using NSubstitute;
+using NUnit.Framework;
 
 namespace ATM.Integration.Test
 {
