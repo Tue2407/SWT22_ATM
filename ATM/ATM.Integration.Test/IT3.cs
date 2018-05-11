a79344550b93bbcfb38b63ad02b31025428b70c3
Tue Jensen <the_tue@hotmail.com>
2018-05-11 12:35:41 +0200

    Added SetUp Frame, ready to begin testing.

--- ATM/ATM.Integration.Test/IT3.cs
+++ ATM/ATM.Integration.Test/IT3.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using ATMClasses;
+using NSubstitute;
+using NUnit.Framework;
 
 namespace ATM.Integration.Test
 {
-    class IT3
+    [TestFixture]
+    public class IT3
     {
+        // Bottom-up. 
+
+        // Husk at include de nødvendige pakker, til Nsubstitute og NUnit.
+
+        // Interface SetUp goes here. 
+
+        [SetUp]
+        public void SetUp()
+        {
+            //SetUp
+        }
+
+        [Test]
+        public void ThisIsATest()
+        {
+            //Template Test Case. 
+        }
     }
 }
