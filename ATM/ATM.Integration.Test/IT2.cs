0caa83deeac288d557313d5e09f3558fbaad82a7
Tue Jensen <the_tue@hotmail.com>
2018-05-11 10:55:38 +0200

    Added IT2

---
+++ ATM/ATM.Integration.Test/IT2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM.Integration.Test
+{
+    class IT2
+    {
+        // Buttom-Up
+    }
+}
