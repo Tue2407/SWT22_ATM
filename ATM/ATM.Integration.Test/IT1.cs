cf9ade416846eff4439b6cbaa096c76196453694
DPH <DPH1108@gmail.com>
2018-05-14 18:40:50 +0200

    Første IT er blevet sat og lavet angående Logger, den forholder sig til både Separation og TrackData. Husk at alle vigtige dependencies skal tilsættes hvis der bruges Update klassen som Separation er Afhænging af.

--- ATM/ATM.Integration.Test/IT1.cs
+++ ATM/ATM.Integration.Test/IT1.cs
@@ -1,35 +1,100 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using ATMClasses;
+using ATMClasses.Data;
+using ATMClasses.Interfaces;
+using ATMClasses.Proximity_Detection;
+using ATMClasses.TrackUpdate;
 using NSubstitute;
+using NSubstitute.Extensions;
 using NUnit.Framework;
+
+// Bottom-up. 
+
+//Vi starter med systemets logger på venstre bund af dependency diagrammet
+//Logger har 2 dependencies, Integrationsmæssigt med SeparationsEvent.Collision, og indholdsmæssigt med TrackData.
+//Selve Testen forgår på Loggeren om den virker med de 2 integrationsdele.
 
 namespace ATM.Integration.Test
 {
     [TestFixture]
     public class IT1
     {
-        // Bottom-up. 
 
-        // Husk at include de nødvendige pakker, til Nsubstitute og NUnit.
+        private Logger _uut;
+        private ISeparation _Event;
+        private TrackData _Track1;
+        private TrackData _Track2;
+        private ICalcDistance _CalcDistance;
+        private IUpdate _Update;
+        private ITrackDecoding _decoder;
+        private ICalcVelocity _calcVelocity;
+        private ICalcCourse _calcCourse;
+        private IMonitors _monitor;
+        private TrackDataEventArgs _fakeTransponderData;
 
-        // Interface SetUp goes here. 
+        private List<ITracks> _Tracklist;
 
         [SetUp]
         public void SetUp()
         {
-            //SetUp
-            Assert.AreEqual(2+2,4);
+            _decoder = Substitute.For<ITrackDecoding>();
+            //Rigtige Klasser
+            _uut = new Logger();
+            _Event = new SeparationEvent();
+            _Track1 = new TrackData(){Tag = "TAG1", X = 1000, Y = 2000, Altitude = 3000 , Course = 30 , Velocity = 23, FormattedTimestamp = "14-05-2018 17:18 53 1111" };
+            _Track2 = new TrackData(){ Tag = "TAG2", X = 1000, Y = 2000, Altitude = 3000, Course = 30, Velocity = 23, FormattedTimestamp = "14-05-2018 17:18 53 1111" };
+
+            _Update = new Update(_decoder);
+            //Lister
+
+            _Tracklist = new List<ITracks>(){_Track1, _Track2};
+            _fakeTransponderData = new TrackDataEventArgs(_Tracklist);
+
+            //Substitueret
+            _CalcDistance = Substitute.For<ICalcDistance>();
+            _calcCourse = Substitute.For<ICalcCourse>();
+            _calcVelocity = Substitute.For<ICalcVelocity>();
+            _monitor = Substitute.For<IMonitors>();
+
+            _decoder.TrackDataReadyForCalculation += (o, args) => _Tracklist = args.TrackData;
         }
 
-        [Test]
-        public void ThisIsATest()
+        public void Action()
         {
-            //Template Test Case. 
+            //Går ind i update løkken der finder ud af om de har kollision
+
+            _Update.TrackCalculated(_monitor, _CalcDistance, _calcCourse, _calcVelocity, _uut, _Event, _Tracklist);
         }
+        private void RaiseFakeEvent()
+        {
+            // Hæv eventet hvis _decoder har fået hævet flaget, indsæt den falske liste
+            _decoder.TrackDataReadyForCalculation += Raise.EventWith(_fakeTransponderData);
+
+        }
+
+        
+        [Test]
+        public void If_SeparationEvent_Is_True_Logger_Starts_And_Show_That_TrackData_Is_Used()
+        {
+            //Man hæver eventet og logger bliver gjort her
+            Action(); //Logger og Tracks bliver initialiseret og sat her
+            RaiseFakeEvent(); //Eventet sker
+
+            using (var stream = new MemoryStream())
+            using (var writer = new StreamWriter(stream))
+            {
+                string[] lines = System.IO.File.ReadAllLines(@"SeparatationEventLog.txt");
+
+                Assert.AreEqual("Timestamp: 14-05-2018 17:18 53 1111\tTAG1 and TAG1 are breaking separation rules", lines[0]);
+            }
+        }
+        
     }
 }
